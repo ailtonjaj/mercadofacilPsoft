1e2d4d6e36650dbe0af19a0043357d236233427d
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2023-03-31 17:08:39 -0300

    .

---
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/Lote.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.mercadofacil;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Lote {
+
+    private Long id;
+    private Produto produto;
+    private int numeroDeItens;
+}
