28d8de5e554ea31c2796eae890dcdb877c83e223
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2023-03-31 17:05:13 -0300

    .

---
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/Produto.java
@@ -0,0 +1,15 @@
+package com.ufcg.psoft.mercadofacil;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private double preco;
+    private String codigoBarra;
+    private String fabricante;
+}
